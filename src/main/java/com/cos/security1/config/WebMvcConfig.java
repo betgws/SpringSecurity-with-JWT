afcad5766539a2af4bbe2f5363a19fa61e9e8ed4
dayeong <dayeongyun184@gmail.com>
2024-03-14 18:18:06 +0900

    시큐리티 권한처리

--- src/main/java/com/cos/security1/config/WebMvcConfig.java
+++ src/main/java/com/cos/security1/config/WebMvcConfig.java
@@ -4,6 +4,8 @@ package com.cos.security1.config;
 import com.samskivert.mustache.Mustache;
 import org.springframework.boot.web.servlet.view.MustacheViewResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
