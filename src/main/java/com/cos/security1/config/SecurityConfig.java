d3436d298ee898569fd61d2e3b9d8874c32a525e
dayeong <dayeongyun184@gmail.com>
2024-03-16 13:02:59 +0900

    구글 로그인 후처리

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.cos.security1.config;
 
 
+import com.cos.security1.config.oauth.PrincipalOauthUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,6 +17,9 @@ import org.springframework.security.config.annotation.web.configurers.CsrfConfig
 @EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터체인에 등록됨
 @EnableGlobalMethodSecurity(securedEnabled = true ,prePostEnabled = true) //secured 어노테이션 활성화 preAutho 머시기 활성화
 public class SecurityConfig  {
+
+    @Autowired
+    private PrincipalOauthUserService principalOauthUserService;
 
 
     @Bean//해당  메서드의 리턴되는 오브젝트를 IOC로 등록해줌
@@ -33,7 +38,17 @@ public class SecurityConfig  {
 
                         .anyRequest().permitAll()
 
-        ).oauth2Login(oauth2-> oauth2.loginPage("/loginForm")); // 구글 로그인이 완료된 후 후처리가 필요함
+        );
+
+            http.oauth2Login(oauth2-> oauth2.loginPage("/loginForm")
+                    .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(principalOauthUserService) ));
+        // 구글 로그인이 완료된 후 후처리가 필요함
+        // tip oauth 외부 라이브러리를 쓰면 코드를 받는게 아니라 바로 엑세스 토큰+사용자프로필 정보를 받음
+        // 1. 코드 받기(인증O), 2. 엑세스토큰(권한),
+        //3.사용자 프로필 정보 가겨오기 4. 그정보를 토대로 바로 회원가입 하기도함
+        // 4-2 (이메일,전화번호,이름,아이디) 쇼핑몰 -> (집주소),백화점몰 -> (등급) 등 추가적인 정보 필요
+
+
 
         http.formLogin(formLogin->formLogin
                 .loginPage("/loginForm")
