3bdf6a038ff4d60e5e498dbbc5a4ce9b723152e2
dayeong <dayeongyun184@gmail.com>
2024-05-05 17:09:08 +0900

    jwt 심화

---
+++ src/main/java/com/example/springjwt/entity/RefreshEntity.java
@@ -0,0 +1,25 @@
+package com.example.springjwt.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Entity
+@Getter
+@Setter
+public class RefreshEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String refresh;
+    private String expiration;
+}
